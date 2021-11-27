b8003947022481aa2ef4ce3961119eff65f8bcb6
Fatih Bektaş <fatih.bektass@gmail.com>
2021-11-28 02:37:31 +0300

    added tables

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:async';
 
-void main(List<String> args) {
+import 'model/dog_model.dart';
+
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'doggie_database.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+      );
+    },
+    version: 1,
+  );
+
+  // insert dog to table
+  Future<void> insertDog(Dog dog) async {
+    final db = await database;
+
+    await db.insert(
+      'dogs',
+      dog.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // retrieve dogs from the table
+  Future<List<Dog>> dogs() async {
+    final db = await database;
+
+    final List<Map<String, dynamic>> maps = await db.query('dogs');
+
+    // Convert the List<Map<String, dynamic> into a List<Dog>.
+    return List<Dog>.generate(maps.length, (i) {
+      return Dog(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        age: maps[i]['age'],
+      );
+    });
+  }
+
+  // update dog
+  Future<void> updateDog(Dog dog) async {
+    final db = await database;
+
+    await db.update(
+      'dogs',
+      dog.toMap(),
+      where: 'id=?',
+      whereArgs: [dog.id],
+    );
+  }
+
+  //delete dog
+  Future<void> deleteDog(int id) async {
+    final db = await database;
+
+    await db.delete(
+      'dogs',
+      where: 'id=?',
+      whereArgs: [id],
+    );
+  }
+
+  // create a dog an add it to the dogs table
+  var fido = Dog(id: 0, name: 'fido', age: 12);
+  insertDog(fido);
+
+  print(await dogs());
+
+  fido = Dog(id: fido.id, name: fido.name, age: fido.age + 12);
+
+  await updateDog(fido);
+
+  print(await dogs());
+
+  await deleteDog(fido.id);
+
+  print(await dogs());
+
   runApp(const MyApp());
 }
 
---
+++ lib/model/dog_model.dart
@@ -0,0 +1,22 @@
+class Dog {
+  final int id;
+  final String name;
+  final int age;
+
+  Dog({
+    required this.id,
+    required this.name,
+    required this.age,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+    };
+  }
+
+  @override
+  String toString() => 'Dog(id: $id, name: $name, age: $age)';
+}
